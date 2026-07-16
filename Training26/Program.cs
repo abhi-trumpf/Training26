283ee0ce1c49423dda88170df986af411ddca003
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-07-16 13:13:21 +0530

    TO3 final execution

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -12,17 +12,20 @@ namespace newconsole1 {
 			for (int i = 0; i < 8; i++) {
 				input1[i] = int.Parse(Console.ReadLine());
 			}*/
+         List<int> gt = new List<int> ();
          for (int i = 0; i < array.Length; i++) {
             int x = array[i];
             for (int j = i + 1; j < array.Length; j++) {
                int z = j - i;
                int y = array[j];
                if (y < x) {
-                  Console.WriteLine ($"The next cooler day after {x} is {y} (first cooler day) - wait ({z}) days");
+                  //Console.WriteLine ($"The next cooler day after {x} is {y} (first cooler day) - wait ({z}) days");
+                  gt.Add(z);
                   break;
                }
             }
          }
+         Console.WriteLine ($"Output: [{string.Join (", ", gt)}]");
       }
       public static void Main (string[] args) {
          coolerDay ();
